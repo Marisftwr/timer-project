dc13ff9413cc8cfdef87c3368cab8415414ec2f7
Anna Beatriz Waldheim <153030185+annabwaldheim@users.noreply.github.com>
2025-05-30 02:21:27 -0300

    add troca de modos

--- context/ModoContext.js
+++ context/ModoContext.js
@@ -1,1 +1,17 @@
+import { createContext, useContext, useState } from 'react';
 
+const opcaoContext = createContext();
+
+export function ModoContagem ({ children }) {
+    // cronometro = false e temporizador = true
+  const [opcaoButton, setOpcaoButton] = useState(false);
+  return (
+    <opcaoContext.Provider value={{ opcaoButton, setOpcaoButton }}>
+      {children}
+    </opcaoContext.Provider>
+  );
+}
+
+export function useModo() {
+  return useContext(opcaoContext);
+}
