b6f757ac5edf0a5baad7f2f8f3008abea352df7e
Anna Beatriz Waldheim <153030185+annabwaldheim@users.noreply.github.com>
2025-05-30 02:13:01 -0300

    add troca de modos

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ModoContagem } from './context/ModoContext';
 import Home from './Pages/Home'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home />
-      <StatusBar style="auto" />
-    </View>
+    <ModoContagem>
+      <View style={styles.container}>
+        <Home />
+        <StatusBar style="auto" />
+      </View>
+    </ModoContagem>
   );
 }
 
